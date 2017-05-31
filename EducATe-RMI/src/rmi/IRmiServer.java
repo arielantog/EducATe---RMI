2d1d38e5c3629c3a888afc13813ce4327229cb62
arielantog <arielantog@gmail.com>
2017-05-30 21:40:20 -0300

    Se agregó el RMI

---
+++ EducATe-RMI/src/rmi/IRmiServer.java
@@ -0,0 +1,13 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IRmiServer extends Remote {
+	//public static final String serviceName = "rmi";
+	//public static final String url = "localhost/" + serviceName;
+	public static final String url = "localhost/rmi";
+
+	public int nuevoAlumno(String tipoDocumento, int nroDocumento, String nombre, String apellido) throws RemoteException;
+	
+}
