20c5f7db1bcb75e1807850948ef4d069a158841b
arielantog <arielantog@gmail.com>
2017-06-01 13:30:19 -0300

    Conectar juego con BackEnd

--- EducATe-RMI/src/rmi/IRmiServer.java
+++ EducATe-RMI/src/rmi/IRmiServer.java
@@ -4,8 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IRmiServer extends Remote {
-	//public static final String serviceName = "rmi";
-	//public static final String url = "localhost/" + serviceName;
 	public static final String url = "localhost/rmi";
 
 	public int nuevoAlumno(String tipoDocumento, int nroDocumento, String nombre, String apellido) throws RemoteException;
