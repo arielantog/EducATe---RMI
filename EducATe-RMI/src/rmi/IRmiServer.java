c9a6948636b90ee72afafca49b416c713ff7acfc
Turco <Turco@10.211.55.3>
2017-06-20 11:07:45 -0300

    cracion de dtos

--- EducATe-RMI/src/rmi/IRmiServer.java
+++ EducATe-RMI/src/rmi/IRmiServer.java
@@ -3,9 +3,22 @@ package rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import dto.AlumnoDTO;
+import dto.DocenteDTO;
+
 public interface IRmiServer extends Remote {
 	public static final String url = "localhost/rmi";
 
-	public int nuevoAlumno(String tipoDocumento, int nroDocumento, String nombre, String apellido) throws RemoteException;
+	public int nuevoAlumno(String tipoDocumento, int nroDocumento, String nombre, String apellido, String usuario, String password, String mail) throws RemoteException;
+
+	public int nuevoDocente(String tipoDocumento, int nroDocumento, String nombre, String apellido, String password, String mail) throws RemoteException;
+
+	//public int loginDocente(String tipoDocumento, int nroDocumento, String password) throws RemoteException;
+
+	//public DocenteDTO loginDocente(int idDocente) throws RemoteException;
+	
+	public DocenteDTO loginDocente(String tipoDocumento, int nroDocumento, String password) throws RemoteException;
+
+	public AlumnoDTO loginAlumno(String usuario, String password)throws RemoteException;
 	
 }
