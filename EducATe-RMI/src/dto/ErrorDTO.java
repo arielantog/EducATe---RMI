f77bbd90e966ce677e5506e3156e97943d4fab8a
arielantog <arielantog@gmail.com>
2017-09-28 18:40:26 -0300

    Login incorrecto Alumno con un solo index

---
+++ EducATe-RMI/src/dto/ErrorDTO.java
@@ -0,0 +1,15 @@
+package dto;
+
+public class ErrorDTO {
+	private String mensaje;
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+
+}
