8907135ad56d52320f16f9f680ff9a865a5a638e
arielantog <arielantog@gmail.com>
2017-09-28 23:17:52 -0300

    Errores de login de Alumnos y Docentes con throws RemoteException

--- EducATe-RMI/src/dto/ErrorDTO.java
+++ EducATe-RMI/src/dto/ErrorDTO.java
@@ -1,8 +1,26 @@
 package dto;
 
 public class ErrorDTO {
+	private int codigo;
 	private String mensaje;
 
+	public ErrorDTO(int codigo, String mensaje) {
+		this.codigo = codigo;
+		this.mensaje = mensaje;
+	}
+	
+	public ErrorDTO(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
 	public String getMensaje() {
 		return mensaje;
 	}
@@ -10,6 +28,8 @@ public class ErrorDTO {
 	public void setMensaje(String mensaje) {
 		this.mensaje = mensaje;
 	}
+
+
 
 
 }
