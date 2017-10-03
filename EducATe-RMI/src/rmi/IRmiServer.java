4f395562fd1ad5ba685adb4525db5581174fe8df
arielantog <arielantog@gmail.com>
2017-10-02 21:16:35 -0300

    Excepciones en mi avatar , error de duplicado en nuevo alumno.

--- EducATe-RMI/src/rmi/IRmiServer.java
+++ EducATe-RMI/src/rmi/IRmiServer.java
@@ -16,10 +16,6 @@ public interface IRmiServer extends Remote {
 	public AlumnoDTO nuevoAlumno(String tipoDocumento, int nroDocumento, String nombre, String apellido, String usuario, String password, String mail) throws RemoteException;
 
 	public DocenteDTO nuevoDocente(String tipoDocumento, int nroDocumento, String nombre, String apellido, String password, String mail) throws RemoteException;
-
-	//public int loginDocente(String tipoDocumento, int nroDocumento, String password) throws RemoteException;
-
-	//public DocenteDTO loginDocente(int idDocente) throws RemoteException;
 	
 	public DocenteDTO loginDocente(String tipoDocumento, int nroDocumento, String password) throws RemoteException;
 
