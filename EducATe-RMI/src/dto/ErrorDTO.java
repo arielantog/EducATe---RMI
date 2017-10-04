5c7826fa4a34fb63953fc1dc5992873a87a3db76
Turco <Turco@10.211.55.3>
2017-10-04 00:11:36 -0300

    Descripcion de los codigos de error

--- EducATe-RMI/src/dto/ErrorDTO.java
+++ EducATe-RMI/src/dto/ErrorDTO.java
@@ -4,6 +4,16 @@ public class ErrorDTO {
 	private int codigo;
 	private String mensaje;
 
+	/*** CODIGOS DE ERROR ***
+	 * 1 - Se intenta registrar un Alumno ya cargado
+	 * 2 - Se intenta registrar un Docente ya cargado
+	 * 3 - Se intenta alimentar el avatar cuando no tiene los puntos necesarios/no tiene hambre/esta muerto
+	 * 4 - Se intenta evolucionar el avatar cuando esta muerto/no tiene la vida suficiente/no tiene los puntos suficientes
+	 * 5 - Se intenta revivir el avatar cuando lo tiene los puntos necesarios
+	 * 6 - 
+	 */
+	
+	
 	public ErrorDTO(int codigo, String mensaje) {
 		this.codigo = codigo;
 		this.mensaje = mensaje;
